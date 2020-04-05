547b86af8c758c6e23909f58263df9e0d6ea9e7b
jlam <jennylamwork@gmail.com>
2020-04-05 16:49:14 -0700

    created connection and schema

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS empTracker_db;
+
+CREATE DATABASE empTracker_db;
+
+USE empTracker_db;
+
+CREATE TABLE department(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
+);
--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-const inquirer = require('inquirer').createPromptModule()
+const prompt = require('inquirer').createPromptModule()
 const mysql2 = require('mysql2')
+
+const connection = mysql2.createConnection({
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'Suger511!',
+  database: 'empTracker_db'
+})
